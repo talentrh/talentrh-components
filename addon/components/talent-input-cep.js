a1f9a7551e63628d6ecc50ba758be5c6574ca603
Bruno Haveroth <bruno.haveroth@viasoft.com.br>
2017-10-06 13:40:41 -0300

    Adiciona componente de input para cpf

---
+++ addon/components/talent-input-cep.js
@@ -0,0 +1,83 @@
+import Ember from 'ember';
+import layout from '../templates/components/talent-input-cep';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: 'talent-input-validation talent-input-cep',
+
+  didInsertElement() {
+		this.$('input').inputmask({ mask: '99.999-999', autoUnmask: true });
+    this.validate();
+	},
+
+  onChangeValue: Ember.observer('value', function() {
+    this.validate();
+  }),
+
+  validate() {
+    let value = this.get('value');
+    let buttonComplete = this.get('buttonComplete');
+    if (!value) { return; }
+
+    if (value.length < 8) { return this.setEffect(false); }
+
+    return this.setEffect(true);
+  },
+
+  setEffect(isValid) {
+    let onUpdateStatus = this.get('onUpdateStatus');
+    let buttonComplete = this.get('buttonComplete');
+
+    if (isValid) {
+      Ember.$(this.element).removeClass('talent-input-validation-error');
+			Ember.$(this.element).addClass('talent-input-validation-success');
+
+      if (buttonComplete) {
+        this.set('showButtonComplete', true);
+      }
+    }
+    else {
+      Ember.$(this.element).removeClass('talent-input-validation-success');
+      Ember.$(this.element).addClass('talent-input-validation-error');
+
+      this.set('showButtonComplete', false);
+    }
+
+    if (onUpdateStatus) {
+      this.sendAction('onUpdateStatus', 'cepValid', isValid);
+    }
+  },
+
+  actions: {
+    findCompleteData() {
+      let value = this.get('value');
+      if (!value) { return; }
+
+      this.set('searching', true);
+
+      Ember.$.ajax({
+        url: `https://viacep.com.br/ws/${value.replace(/([.||-])/g, "")}/json/`,
+        method: 'GET'
+      }).then(response => {
+        this.set('searching', false);
+
+        if (response.erro) {
+          swal('Ops', 'Não foi possível completar automaticamente.', 'info');
+          return;
+        }
+
+        this.set('address', response.logradouro);
+        this.set('district', response.bairro);
+        this.set('complement', response.complemento);
+
+        let loadCity = this.get('loadCity');
+        if (loadCity) {
+          this.sendAction('loadCity', response);
+        }
+      }).catch(()=> {
+        this.set('searching', false);
+        swal('Ops', 'Não foi possível completar automaticamente.', 'info');
+      });
+    }
+  }
+});
