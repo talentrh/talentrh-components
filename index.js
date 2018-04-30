105fd8aeaa50982d6f576a2fe0b0ba6439dc824c
Luiz Carraro <lacarraro@gmail.com>
2018-04-30 10:35:08 -0300

    Adiciona imports

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ module.exports = {
   name: 'talentrh-components',
   isDevelopingAddon() {
     return true;
+  },
+  included: function (app) {
+    this._super.included(app);
+
+    app.import('vendor/style.css');
+
+    /*--- import select2 ---*/
+    app.import('node_modules/select2/dist/css/select2.css');
+    app.import('node_modules/select2/dist/js/select2.min.js');
+    /*--- import select2 ---*/
+
+    /*--- import sweetalert ---*/
+    app.import('node_modules/sweetalert2/dist/sweetalert2.min.js');
+    app.import('node_modules/sweetalert2/dist/sweetalert2.css');
+    /*--- import sweetalert ---*/
   }
 };
