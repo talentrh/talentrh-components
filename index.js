62f75e80a06b18943fb29e627b72412620056625
Bruno Haveroth <bruno.haveroth@gmail.com>
2018-05-23 11:41:37 -0300

    Importa bootstrap4-datetimepicker e vanillajs

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = {
     this._super.included(app);
 
     app.import('vendor/style.css');
+    app.import('vendor/js/vanilla-masker.min.js');
 
     /*--- import select2 ---*/
     app.import('node_modules/select2/dist/css/select2.css');
@@ -19,5 +20,10 @@ module.exports = {
     app.import('node_modules/sweetalert2/dist/sweetalert2.min.js');
     app.import('node_modules/sweetalert2/dist/sweetalert2.css');
     /*--- import sweetalert ---*/
+
+    /*--- import datetimepicker ---*/
+    app.import('node_modules/bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.min.css');
+    app.import('node_modules/bootstrap4-datetimepicker/build/js/bootstrap-datetimepicker.min.js');
+    /*--- import datetimepicker ---*/
   }
 };
