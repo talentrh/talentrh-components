397e17601bf70ec44fe48d14db44475e6dfa2e20
Luiz Carraro <lacarraro@gmail.com>
2018-07-20 09:27:48 -0300

    animated-logo e estrutura scss

---
+++ addon/components/nela-animated-logo.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/nela-animated-logo';
+
+export default Component.extend({
+  layout,
+  classNames: 'animation-logo',
+  animate: true
+});
