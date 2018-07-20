adbe6fdc5e2efb81d2aa072a7694f6ba6d1c3158
Luiz Carraro <lacarraro@gmail.com>
2018-07-20 09:28:59 -0300

    helper truncate-text

---
+++ addon/helpers/truncate-text.js
@@ -0,0 +1,26 @@
+import { helper } from '@ember/component/helper';
+
+export function truncateText(params, hash) {
+  let stripHTML = (str) => str.replace(/<.*?>/g, '');
+  var value = params[0] || '';
+  var len = hash.limit;
+  var out = '';
+
+  if (value !== undefined) {
+    // console.log('Antes: ', value);
+    value = stripHTML(value);
+    // console.log('Depois: ', value);
+    out = value.substr(0, len);
+
+    if (value.length > len) {
+      out += '...';
+    }
+
+  } else {
+    out = '';
+  }
+
+  return out;
+}
+
+export default helper(truncateText);
